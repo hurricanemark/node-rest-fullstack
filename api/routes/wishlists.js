ebb90387e72517268a6be40ce49dfd4b533ba2ad
Mark Nguyen <marknltv@comcast.net>
2019-11-09 15:29:50 -0800

    exclude node_modules

--- api/routes/wishlists.js
+++ api/routes/wishlists.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Wishlist = require('../models/wishlists');
+const Product = require('../models/products');
 
 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Wishlist was fetched'
-    });
+    Wishlist.find()
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                wishlist: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        pricequote: doc.pricequote,
+                        request: {
+                            type: 'GET',
+                            url: process.env.BASE_URL + 'wishlists/' + doc._id
+                        }
+                    }
+                })
+            });
+        })
+        .catch(err => {
+            error: err
+        })
 });
 
 
@@ -18,14 +39,14 @@ router.post('/', (req, res, next) => {
 
 router.get('/:wishlistId', (req, res, next) => {
     res.status(200).json({
-        message: 'Wishlist details for', 
+        message: 'Wishlist details for',
         wishlistId: req.params.wishlistId
     });
 });
 
 router.delete('/:wishlistId', (req, res, next) => {
     res.status(200).json({
-        message: 'Wishlist deleted for', 
+        message: 'Wishlist deleted for',
         wishlistId: req.params.wishlistId
     });
 });
